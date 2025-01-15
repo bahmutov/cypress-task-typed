8cb07839dad17ea0dbdd931e40667a35c3442aac
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-15 09:32:23 -0500

    add link to the blog post

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cypress-task-typed
 
 > Example specifying types for cy.task command events
+
+📝 Read the blog post [Type cy.task Command](https://glebbahmutov.com/blog/typed-cy-task/)
--- cypress/index.d.ts
+++ cypress/index.d.ts
@@ -1,5 +1,8 @@
 declare namespace Cypress {
   interface Chainable {
+    /**
+     * Adds two numbers in Node and yields their sum
+     */
     task(
       event: 'add',
       options: { a: number; b: number },
